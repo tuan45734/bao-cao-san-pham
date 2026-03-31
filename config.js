47b48717f01b0e97afa5f1077a33fae169519b48
tuan45734 <tuanhvph45734@fpt.edu.vn>
2026-03-31 10:24:02 +0700

    t

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const CONFIG = {
         'accept': 'application/json',
         'Authorization': 'Basic NjlhZTZlNmM4YTY0NjVmNDFlNTNhZmI0OjFuYzFnc3J1N2p2Ym10eTdncGV5NWk='
     },
-    PAGE_SIZE: 1000,
+    PAGE_SIZE: 3000,  // Đã thay đổi từ 1000 lên 3000
     MAX_RETRIES: 3,
     RETRY_DELAY: 2000,
     PAGE_DELAY: 1000
